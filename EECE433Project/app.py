8be2e3ab13e4d005bb8abc23f6560f263513ff60
antoineabf <ana70@mail.aub.edu>
2024-04-12 13:17:27 +0300

    Login and register done

--- EECE433Project/app.py
+++ EECE433Project/app.py
@@ -1,11 +1,93 @@
-from flask import Flask
+import datetime
+import jwt
+
+from flask import Flask, render_template, request, session, redirect
+import psycopg2
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+conn = psycopg2.connect(
+    database="gym", user="postgres",
+    password="mysql123", host="127.0.0.1", port="5432"
+)
+
+SECRET_KEY = "b'|\xe7\xbfU3`\xc4\xec\xa7\xa9zf:}\xb5\xc7\xb9\x139^3@Dv'"
+
+
+def create_token(user_id):
+    payload = {
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=4),
+        'iat': datetime.datetime.utcnow(),
+        'sub': user_id
+    }
+    return jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm='HS256'
+    )
+
+def extract_auth_token(authenticated_request):
+    auth_header = authenticated_request.headers.get('Authorization')
+    if auth_header:
+        return auth_header.split(" ")[1]
+    else:
+        return None
+
+
+def decode_token(token):
+    payload = jwt.decode(token, SECRET_KEY, 'HS256')
+    return payload['sub']
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    token = session.get('token')
+    return render_template("index.html", token=token)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        cursor = conn.cursor()
+        cursor.execute("select mid, pass from member where email =%s", (email,))
+        data = cursor.fetchone()
+        if data is None:
+            return "error"
+        if data[1] == password:
+            session['token'] = create_token(data[0])
+            return redirect("/")
+    return render_template("login.html")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        fname = request.form["fname"]
+        lname = request.form["lname"]
+        email = request.form["email"]
+        password = request.form["password"]
+        gender = request.form["gender"]
+        dob = request.form["dob"]
+        contact = request.form["contact"]
+        cursor = conn.cursor()
+        cursor.execute("""INSERT INTO MEMBER (FNAME, LNAME, GENDER, DOB, EMAIL, PASS, CONTACT, JOININGDATE)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, 'now()')""",
+                       (fname, lname, gender, dob, email, password, contact))
+        conn.commit()
+        cursor.execute("select mid from member where email =%s", (email,))
+        data = cursor.fetchone()
+        session['token'] = create_token(data[0])
+        return redirect("/")
+    return render_template("register.html")
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect("/")
 
 
 if __name__ == '__main__':
