c50830e6fa1348155c21dc96ffaab380c88571fd
Abdullatif Itani <awi03@mail.aub.edu>
2024-04-21 19:00:05 +0300

    Added create staff and create class

--- EECE433Project/app.py
+++ EECE433Project/app.py
@@ -9,10 +9,10 @@ import psycopg2
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-conn = psycopg2.connect(
-    database="gym", user="postgres",
-    password="mysql123", host="127.0.0.1", port="5432"
-)
+conn = psycopg2.connect( 
+    database="Gym Management", user="postgres", 
+    password="Abed12345", host="127.0.0.1", port="5432" 
+    )
 
 SECRET_KEY = "b'|\xe7\xbfU3`\xc4\xec\xa7\xa9zf:}\xb5\xc7\xb9\x139^3@Dv'"
 
@@ -64,7 +64,7 @@ def login():
         email = request.form["email"]
         password = request.form["password"]
         cursor = conn.cursor()
-        cursor.execute("select mid, pass from auth where email =%s", (email,))
+        cursor.execute("select mid, pass from member where email =%s", (email,))
         data = cursor.fetchone()
         if data is None:
             return "error"
@@ -108,7 +108,52 @@ def logout():
 @admin_required
 def admin_panel():
     # Admin functionalities
+    if request.method == "POST":
+        if request.form.get("action") == "create_staff":
+            return redirect("/create_staff")
+        elif request.form.get("action") == "create_class":
+            return redirect("/create_class")
     return render_template('admin_panel.html')
+
+@app.route('/create_staff', methods=["GET", "POST"])
+@admin_required
+def create_staff():
+    if request.method == "POST":
+        fname = request.form["fname"]
+        lname = request.form["lname"]
+        gender = request.form["gender"]
+        dob = request.form["dob"]
+        email = request.form["email"]
+        password = request.form["password"]
+        contact = request.form["contact"]
+        salary = request.form["salary"]
+        position = request.form["position"]
+        employment_date = request.form["employment_date"]
+        
+        cursor = conn.cursor()
+        cursor.execute("""INSERT INTO STAFF (FNAME, LNAME, GENDER, DOB, EMAIL, PASS, CONTACT, SALARY, POSITION, EMPLOYMENTDATE)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+                       (fname, lname, gender, dob, email, password, contact, salary, position, employment_date))
+        conn.commit()
+        return redirect("/admin")
+    return render_template("create_staff.html")
+
+
+@app.route('/create_class', methods=["GET", "POST"])
+@admin_required
+def create_class():
+    if request.method == "POST":
+        cname = request.form["cname"]
+        max_cap = request.form["max_cap"]
+        description = request.form["description"]
+        
+        cursor = conn.cursor()
+        cursor.execute("""INSERT INTO CLASS (CNAME, MAX_CAP, DESCRIPTION)
+                        VALUES (%s, %s, %s)""",
+                       (cname, max_cap, description))
+        conn.commit()
+        return redirect("/admin")
+    return render_template("create_class.html")
 
 
 if __name__ == '__main__':
