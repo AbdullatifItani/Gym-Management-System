d4330e314f0dffa60281032a2f455a0464af938d
antoineabf <ana70@mail.aub.edu>
2024-04-26 14:37:58 +0300

    added favicon

--- EECE433Project/app.py
+++ EECE433Project/app.py
@@ -71,11 +71,6 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-
-
 @app.route('/admin')
 @admin_required
 def admin():
--- EECE433Project/db_config.py
+++ EECE433Project/db_config.py
@@ -1,5 +1,5 @@
 DB_NAME = 'gym'
 DB_USER = 'postgres'
-DB_PASSWORD = 'Abed12345'
+DB_PASSWORD = 'mysql123'
 DB_HOST = '127.0.0.1'
 DB_PORT = '5432'
